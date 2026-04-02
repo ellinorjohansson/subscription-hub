837640dad3b4498f2f5e41d8096005b2a7406f49
Ellinor Johansson <ellinor.asaka@gmail.com>
2026-04-02 18:25:44 +0200

    💄(style): changed px and py on buttons

--- src/components/YourSubscription.tsx
+++ src/components/YourSubscription.tsx
@@ -4,16 +4,16 @@ const YourSubscription = () => {
       <div className="flex flex-col md:flex-row justify-between">
         <h2 className="text-2xl mb-3">Your Subscription</h2>
         <div className="inline-flex rounded-lg overflow-hidden bg-gray-50/5">
-          <button className="px-14 md:px-8 py-2 bg-amber-700 cursor-pointer">
+          <button className="px-14 md:px-8 bg-amber-700 cursor-pointer">
             All 7
           </button>
-          <button className="px-14 md:px-8 py-2 cursor-pointer">
+          <button className="px-14 md:px-8 cursor-pointer">
             Active 4
           </button>
-          <button className="px-14 md:px-8 py-2 cursor-pointer">
+          <button className="px-14 md:px-8 cursor-pointer">
             Paused 2
           </button>
-          <button className="px-14 md:px-8 py-2 cursor-pointer">
+          <button className="px-14 md:px-8 cursor-pointer">
             Canceled 1
           </button>
         </div>
