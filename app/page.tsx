bd83baf82758afe330d0fc3a1e5d95e7d6b4ee4c
Ellinor Johansson <ellinor.asaka@gmail.com>
2026-04-02 17:57:25 +0200

    ✨(feature): added your subscription section component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import OverviewSection from "@/src/components/OverviewSection";
 import QuickAddSection from "@/src/components/QuickAddSection";
+import YourSubscription from "@/src/components/YourSubscription";
 
 export default function Home() {
   return (
     <main className="flex flex-col flex-1 font-sans dark:bg-black">
       <OverviewSection />
       <QuickAddSection />
+      <YourSubscription />
     </main>
   );
 }
