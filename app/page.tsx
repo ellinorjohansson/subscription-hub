dff24c5e79dfe61c626ab092e724c90aa390ffff
Ellinor Johansson <ellinor.asaka@gmail.com>
2026-04-02 17:02:48 +0200

    💄(style): style fix for header

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black min-h-1000">
+      test
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 const Header = () => {
   return (
-    <header className="flex flex-row justify-between bg-grey px-30 py-8 items-center shadow-md">
+    <header className="flex flex-row justify-between bg-grey px-30 py-8 items-center shadow-md sticky top-0 z-30 backdrop-blur-sm">
       <div className="flex flex-col">
         <h1 className="text-2xl font-bold text-amber-100">Subtracker</h1>
         <span className="text-sm text-amber-100/80">Manage your subscriptions</span>
       </div>
       <div>
-        <button className="rounded-xl bg-amber-700 px-5 py-2 cursor-pointer">+ Add Subscription</button>
+        <button className="rounded-xl bg-amber-700 px-5 py-2 cursor-pointer text-amber-50 text-sm">+ Add Subscription</button>
       </div>
     </header>
   )
